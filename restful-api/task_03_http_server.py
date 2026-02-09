84081b829977a6035a9fd7acbbd98f4f7ba3a372
salman_ghannam1 <sloome222@gmail.com>
2026-02-09 13:51:10 +0300

    f

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module to implement http.server module"""
+
 import http.server
 import json
 
@@ -7,21 +8,21 @@ import json
 class HTTPHandler(http.server.BaseHTTPRequestHandler):
     """Simple Handler class inherited from BaseHTTPRequestHandler"""
 
+    def _set_headers(self, status=200, content_type='text/plain'):
+        """Reusable method for sending headers"""
+        self.send_response(status)
+        self.send_header('Content-type', content_type)
+        self.end_headers()
+
     def do_GET(self):
         """Method to handle GET requests"""
 
-        # Base case
         if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write('Hello, this is a simple API!'.encode())
+            self._set_headers(content_type='text/plain')
+            self.wfile.write(b"Hello, this is a simple API!")
 
-        # If /data is accessed
         elif self.path == '/data':
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
+            self._set_headers(content_type='application/json')
             data = {
                 "name": "John",
                 "age": 30,
@@ -29,12 +30,9 @@ class HTTPHandler(http.server.BaseHTTPRequestHandler):
             }
             self.wfile.write(json.dumps(data).encode('utf-8'))
 
-        # /status endpoint
         elif self.path == '/status':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write("OK".encode('utf-8'))
+            self._set_headers(content_type='text/plain')
+            self.wfile.write(b"OK")
 
         elif self.path == '/info':
             self._set_headers(content_type='application/json')
@@ -44,15 +42,14 @@ class HTTPHandler(http.server.BaseHTTPRequestHandler):
             }
             self.wfile.write(json.dumps(data).encode('utf-8'))
 
-        # Error 404 :)
         else:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write('404 Not Found'.encode())
+            self._set_headers(status=404, content_type='text/plain')
+            self.wfile.write(b"404 Not Found")
+
 
 if __name__ == '__main__':
     """Server initialization"""
     server_address = ('', 8000)
     httpserver = http.server.HTTPServer(server_address, HTTPHandler)
+    print("Server running at http://localhost:8000")
     httpserver.serve_forever()
