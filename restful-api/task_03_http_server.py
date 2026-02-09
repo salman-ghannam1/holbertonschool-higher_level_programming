06ab51fcf22dce60560fd26abfb60ef274cdaba3
salman_ghannam1 <sloome222@gmail.com>
2026-02-09 14:34:26 +0300

    f

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -46,7 +46,7 @@ class HTTPHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Not Found\n")
+            self.wfile.write(b"404: Not Found\n")
 
 if __name__ == '__main__':
     """Server initialization"""
