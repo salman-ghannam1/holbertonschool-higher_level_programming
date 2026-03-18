dca05de8fbdca3bf424b2d5f1cf617fa91daa470
salman_ghannam1 <sloome222@gmail.com>
2026-03-18 08:17:12 +0300

    f

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const x = Number(process.argv[2]);
 if (Number.isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < x * 3; i++) {
     console.log('C is fun');
   }
 }
