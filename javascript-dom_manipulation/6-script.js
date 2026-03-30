ec05f934e9b0543302dfac7e57af39845ca62f71
salman_ghannam1 <sloome222@gmail.com>
2026-03-30 16:49:35 +0300

    f

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,7 @@
+const character = document.querySelector("#character");
+
+fetch("https://swapi-api.hbtn.io/api/people/5/?format=json")
+    .then(response => response.json())
+    .then(data => {
+        character.textContent = data.name;
+    });
