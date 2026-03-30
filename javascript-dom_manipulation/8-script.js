4ebce97ac27d0da5257ced2aa05dceb593891ff8
salman_ghannam1 <sloome222@gmail.com>
2026-03-30 16:55:24 +0300

    f

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hello = document.querySelector("#hello");
+
+    fetch("https://hellosalut.stefanbohacek.com/?lang=fr")
+        .then(response => response.json())
+        .then(data => {
+            hello.textContent = data.hello;
+        });
+});
