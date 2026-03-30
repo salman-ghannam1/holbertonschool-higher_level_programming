31b26ed4d163937d864ca2fa5b8c8874c4f544fc
salman_ghannam1 <sloome222@gmail.com>
2026-03-30 16:52:34 +0300

    f

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,11 @@
+const list = document.querySelector("#list_movies");
+
+fetch("https://swapi-api.hbtn.io/api/films/?format=json")
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(movie => {
+            const li = document.createElement("li");
+            li.textContent = movie.title;
+            list.appendChild(li);
+        });
+    });
